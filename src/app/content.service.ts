c91004681c777ddd668dba9a1b4ef7d62edf667b
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-20 14:44:14 +0500

    adding service http methods

--- src/app/content.service.ts
+++ src/app/content.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Content } from "./Content";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  readonly txtEditorUrl = "https://localhost:7212/api";
+
+  constructor(private http: HttpClient) { }
+
+  addContent(content: Content): Observable<Content> {
+    return this.http.post<Content>(this.txtEditorUrl + '/PostContent/Create', content);
+  }
+
+  updateContent(id: number, content: Content): Observable<Content> {
+    return this.http.put<Content>(this.txtEditorUrl + `/PostContent/Update/${id}`, content);
+  }
+
+  deleteContent(id: string): Observable<number> {
+    return this.http.delete<number>(this.txtEditorUrl + `/PostContent/Delete/${id}`);
+  }
+
+  getContentList(): Observable<Content[]> {
+    return this.http.get<Content[]>(this.txtEditorUrl + '/PostContent/GetAll');
+  }
+
+  getContentById(id: number): Observable<Content> {
+    return this.http.get<Content>(this.txtEditorUrl + `/PostContent/Get/${id}`);
+  }
+
 }
