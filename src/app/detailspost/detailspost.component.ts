8f5ae7d03a0501275caa0698fc1f6366cd996070
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-20 15:18:05 +0500

    changing detailspost component

--- src/app/detailspost/detailspost.component.ts
+++ src/app/detailspost/detailspost.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from '../content.service';
+import { Content } from './../Content';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-detailspost',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detailspost.component.scss']
 })
 export class DetailspostComponent implements OnInit {
+  res: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private contentservice: ContentService) { }
 
   ngOnInit(): void {
+    let Id = this.route.snapshot.queryParams["Id"]
+    this.GetcontentById(Id);
+  }
+
+  GetcontentById(Id:number)
+  {
+     this.contentservice.getContentById(Id).subscribe((data: any)=>{
+      this.res=data;
+    });
   }
 
 }
