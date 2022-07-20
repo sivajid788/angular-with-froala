64b583d38ff09b64f9cd35c45058fd8a670d7f91
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-20 14:16:11 +0500

    Configuring settings in angular froala app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+// import "froala-editor/js/plugins.pkgd.min.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  public options: Object = {
+    events: {
+      initialized: function() {
+        console.log(this);
+      }
+    },
+    placeholderText: "Start typing..."
+  };
+ 
+  public editorContent: string = 'My Document\'s Title';
   title = 'MyFroalaApp';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
