74fc334b0bf9de3d2ef673389210fd460d1dfe65
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-20 14:34:52 +0500

    configuring tsconfig.json settings

---
+++ src/app/Content.ts
@@ -0,0 +1,5 @@
+export class Content {
+  Id: number;
+  Title: string;
+  Content: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TexteditorComponent } from './texteditor/texteditor.component';
+import { PagecontentComponent } from './pagecontent/pagecontent.component';
+import { DetailspostComponent } from './detailspost/detailspost.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TexteditorComponent,
+    PagecontentComponent,
+    DetailspostComponent
   ],
   imports: [
     BrowserModule,
