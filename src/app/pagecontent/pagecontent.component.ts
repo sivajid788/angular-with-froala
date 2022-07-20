b7ae27f728ef4831ab8a58dd907ec222e36c58df
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-20 15:14:49 +0500

    editing pagecomponent

--- src/app/pagecontent/pagecontent.component.ts
+++ src/app/pagecontent/pagecontent.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ContentService } from 'app/content.service';
 
 @Component({
   selector: 'app-pagecontent',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagecontentComponent implements OnInit {
 
-  constructor() { }
+  res: any = [];
+
+  constructor(private contentservice: ContentService, private router: Router) { }
 
   ngOnInit(): void {
+    this.Getcontent();
+  }
+
+  Getcontent()
+  {
+    this.contentservice.getContentList().subscribe((data)=>{
+      this.res=data;
+      console.log(this.res);
+    })
+  }
+  GetcontentById(id: number)
+  {
+    this.router.navigate(['/Details'], { queryParams: { Id: id } });
   }
 
 }
